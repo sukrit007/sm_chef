3cf23a97d768589cc1a8da835df0a705661ab5f1
sukrit007 <sukrit007@gmail.com>
2011-12-21 11:05:37 +0100

    block_device_ext : init cookbook

---
+++ cookbooks/block_device_ext/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Pearson"
+maintainer_email "sukrit007@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures block_device_ext"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
---
+++ cookbooks/block_device_ext/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: block_device_ext
+# Recipe:: default
+#
+# Copyright 2011, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
