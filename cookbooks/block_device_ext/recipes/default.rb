5fe5e90cc5bc9893d319c9ee0c348c058aa29a36
sukrit007 <sukrit007@gmail.com>
2011-12-26 21:03:38 +0100

    repolace if by case

--- cookbooks/block_device_ext/recipes/default.rb
+++ cookbooks/block_device_ext/recipes/default.rb
@@ -10,9 +10,12 @@
 Chef::Log.info("Block Device Ext :: default for mode: #{node[:block_device_ext][:create_mode]}")
 Chef::Log.info("node #{node[:block_device][:storage_type]}"
 
-if node[:block_device][:storage_type] == "create"
-	include_recipe "block_device::setup_block_device"
-else
-	include_recipe "block_device::do_restore"
+case node[:block_device][:create_mode]
+	when "create" then
+		include_recipe "block_device::setup_block_device"
+	when "restore" then
+		include_recipe "block_device::do_restore"
+	else
+		Chef::Log.error("Option not supported"		
 end
 	
