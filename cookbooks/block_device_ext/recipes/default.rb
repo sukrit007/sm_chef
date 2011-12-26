b21cff7c5839bbc8fb53c6073d9e0b50beddab94
sukrit007 <sukrit007@gmail.com>
2011-12-26 23:27:24 +0100

    Final try

--- cookbooks/block_device_ext/recipes/default.rb
+++ cookbooks/block_device_ext/recipes/default.rb
@@ -11,12 +11,12 @@ Chef::Log.info("Block Device Ext :: default for mode: #{node[:block_device_ext][
 
 
 create_mode = "#{node[:block_device_ext][:create_mode]}"
-#case create_mode
-#	when "create" then
-#		include_recipe "block_device::setup_block_device"
-#	when "restore" then
-#		include_recipe "block_device::do_restore"
-#	else
-#		Chef::Log.error("Option not supported")		
-#end
+case create_mode
+	when "create" then
+		include_recipe "block_device::setup_block_device"
+	when "restore" then
+		include_recipe "block_device::do_restore"
+	else
+		Chef::Log.error("Option not supported")		
+end
 	
