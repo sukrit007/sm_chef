dc5667ab44798d8c7e62be513cbec7deb2c933bb
sukrit007 <sukrit007@gmail.com>
2011-12-27 07:33:37 +0100

    REBOOT -> vg_rightscale scan

--- cookbooks/block_device_ext/recipes/setup.rb
+++ cookbooks/block_device_ext/recipes/setup.rb
@@ -9,18 +9,20 @@
 
 Chef::Log.info("Block Device Ext :: setup for mode: #{node[:block_device_ext][:create_mode]}")
 
-case ENV['RS_REBOOT']
-	when "true" then
-		Chef::Log.info("Skip setup on reboot")	
-	else
-		create_mode = "#{node[:block_device_ext][:create_mode]}"
-		case create_mode
-			when "create" then
-				include_recipe "block_device::setup_block_device"
-			when "restore" then
-				include_recipe "block_device::do_restore"
-			else
-				Chef::Log.error("Option not supported")		
+#Check if volume group vg-rightscale already exists
+system 'lvm lvscan | grep vg-rightscale'
+
+if $?.to_i == 0
+	Chef::Log.info("Volume Group vg-rightscale already exists. Skip setup.")	
+else
+	create_mode = "#{node[:block_device_ext][:create_mode]}"
+	case create_mode
+		when "create" then
+			include_recipe "block_device::setup_block_device"
+		when "restore" then
+			include_recipe "block_device::do_restore"
+		else
+			Chef::Log.error("Option not supported")		
 	end
 end
 	
